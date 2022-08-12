565edbd7110c728eb58d2c4e5601358f02fd27c7
Juanmapp <juanma1694@gmail.com>
2022-08-12 01:10:06 -0300

    Primer commit actividad clase 6

---
+++ clase 6/calculadora.js
@@ -0,0 +1,32 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+function sumar (a, b) {
+    return a + b ; 
+}
+let resultadoSuma = sumar (parseInt(prompt("ingrese su num")), parseInt(prompt("ingrese su 2num")));
+console.log(resultadoSuma);
+
+function restar (c, d) {
+    return c - d ; 
+}
+let resultadoResta = restar (prompt("ingrese su num"), prompt("ingrese su 2num"));
+console.log(resultadoResta);
+
+function multiplicar (e,f) {
+    return e * f;
+}
+let resultadoMultiplicacion = multiplicar(prompt("ingrese su num"), prompt("ingrese su 2num"));
+console.log(resultadoMultiplicacion);
+
+function division (g,h) {
+    return g/h ;
+}
+let resultadoDiv = division(prompt("ingrese su num"), prompt("ingrese su 2num"));
+console.log(resultadoDiv);
+
+let final1raParte = 
+
+console.log(“-------------- Testeo de Operaciones / Calculadora --------------”)
+
+
+
