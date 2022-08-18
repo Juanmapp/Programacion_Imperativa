23ff35e05aa6c18eaacc3a44f7c104dae197c9b6
Juanmapp <juanma1694@gmail.com>
2022-08-18 01:03:36 -0300

    clase 7 y 8

---
+++ Clase 7/actividad.js
@@ -0,0 +1,17 @@
+// const prompt = require("prompt-sync")({ sigint: true });
+
+// let edad = -22; 
+
+// if(edad < 18 && edad > 0 ) {
+// console.log("No puede pasar al bar.")
+// } else if (edad < 21 && edad > 0 ){
+//     console.log("Puede pasar al bar, pero no puede tomar alcohol.")
+// }else if ( edad < 0){
+// console.log("Error, edad inválida. Por favor ingrese un número válido.")
+// } else {
+// console.log("Puede pasar al bar y tomar alcohol.")
+// }
+
+for(let i=0; i<=5; i++) {
+    console.log(i);
+}
