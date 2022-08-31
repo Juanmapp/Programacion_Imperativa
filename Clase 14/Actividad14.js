dfdcaf0fda022278cbe96da0a47c520a34b17504
Juanmapp <juanma1694@gmail.com>
2022-08-31 18:16:17 -0300

    actividad clase 14

---
+++ Clase 14/Actividad14.js
@@ -0,0 +1,262 @@
+// A partir de el siguiente array de edades nos solicitan realizar lo siguiente:
+
+
+
+
+
+
+
+
+
+const edades = [11, 12, 15, 18, 25, 22, 10, 33, 18, 5];
+
+const edadesGrupo1 = [21, 12, 15, 18, 25];
+const edadesGrupo2 = [2, 11, 54, 63, 24];
+// Obtener en un nuevo array las edades menores a 18.
+function edadesMenores(array) {
+    let menores = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 18 ) {
+            menores.push(array[i])
+        }
+    }
+    return menores
+
+}
+console.log(edadesMenores(edades))
+
+// Obtener en un nuevo array las edades mayor o igual a 18.
+
+function edadesMayores(array) {
+    let mayores = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]>= 18) {
+            mayores.push(array[i])
+        }
+    }
+    return mayores
+}
+console.log(edadesMayores(edades))
+
+// Obtener en un nuevo array las edades igual a 18.
+
+function edades18(array) {
+    let edad18 = []
+    for (let i = 0; i < array.length; i++){
+        if (array[i] === 18) {
+            edad18.push(array[i])
+        }
+    }
+    return edad18
+}
+console.log(edades18(edades))
+
+// Obtener el menor.
+
+function edadMenor(array) {
+    return Math.min(...array)
+}
+console.log(edadMenor(edades))
+
+// Obtener el mayor. 
+
+function edadMayor(array) {
+    return Math.max(...array)
+}
+console.log(edadMayor(edades))
+
+// Obtener el promedio de edades.
+
+function promedio(array) {
+    let promediando = 0
+    for (let i = 0; i < array.length; i++) {
+        promediando = promediando + array[i]
+    }
+    return promediando / array.length
+}
+console.log(promedio(edades))
+
+// Incrementar en 1 todas las edades.
+
+function edadesIncrem(array) {
+    let incrementadas = []
+    for (let i = 0 ; i < array.length; i++) {
+        incrementadas.push(array[i] + 1 )
+    }
+    return incrementadas
+}
+console.log(edadesIncrem(edades))
+console.log("--------------------------------")
+console.log("--------------------------------")
+console.log("--------------------------------")
+
+
+
+const arrayCuentas =
+[
+    {
+      titular: "Arlene Barr",
+      estaHabilitada: false,
+      saldo: "$3,253.40",
+      edadTitular: 33,
+      tipoCuenta: "sueldo"
+    },
+    {
+      titular: "Roslyn Torres",
+      estaHabilitada: false,
+      saldo: "$3,229.45",
+      edadTitular: 27,
+      tipoCuenta: "sueldo"
+    },
+    {
+      titular: "Cleo Lopez",
+      estaHabilitada: true,
+      saldo: "$1,360.19",
+      edadTitular: 34,
+      tipoCuenta: "corriente"
+    },
+    {
+      titular: "Daniel Malone",
+      estaHabilitada: false,
+      saldo: "$3,627.12",
+      edadTitular: 30,
+      tipoCuenta: "sueldo"
+    },
+    {
+        titular: "Ethel Leon",
+        estaHabilitada: true,
+        saldo: "$1,616.52",
+        edadTitular: 34,
+        tipoCuenta: "sueldo"
+      },
+      {
+        titular: "Harding Mitchell",
+        estaHabilitada: true,
+        saldo: "$1,408.68",
+        edadTitular: 25,
+        tipoCuenta: "corriente"
+      }
+    ]
+    // Obtener un nuevo array de cuentas cuyas edades sean menores a 30.
+
+function cuentasMenoresA30(array) {
+    let cuentasM30 = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].edadTitular < 30) {
+            cuentasM30.push(array[i])
+        }
+    }
+    return cuentasM30
+}
+console.log("--------------------------------")
+console.log(cuentasMenoresA30(arrayCuentas))
+
+    // Obtener un nuevo array de cuentas cuyas edades sean mayor o igual a 30.
+function cuentasMayoresA30(array) {
+    let cuentasMay30 = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].edadTitular >= 30) {
+            cuentasMay30.push(array[i])
+        }
+    }
+    return cuentasMay30
+}
+console.log("--------------------------------")
+console.log(cuentasMayoresA30(arrayCuentas))
+    // Obtener un nuevo array de cuentas cuyas edades sean igual a 30.
+
+    function cuentasIgualesA30(array) {
+        let cuentasIgu30 = []
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].edadTitular === 30) {
+                cuentasIgu30.push(array[i])
+            }
+        }
+        return cuentasIgu30
+    }
+console.log("--------------------------------")
+console.log(cuentasIgualesA30(arrayCuentas))
+console.log("--------------------------------")
+    // Obtener la cuenta con el titular de la misma más joven.
+
+function masJoven(array) {
+        let edadMasJoven = []
+    for (let i = 0; i < array.length; i++ ) {
+        edadMasJoven.push(array[i].edadTitular)
+    }
+    for (let i = 0; i < array.length; i++ ) {
+        if (array[i].edadTitular === Math.min(...edadMasJoven)){    
+    return array[i]
+}
+}
+}
+console.log(masJoven(arrayCuentas))
+console.log("ejerciciooo")
+    // Obtener un nuevo array por cada tipo de cuenta.
+
+function tiposDeCta(array) {
+    let sueldo = []
+    let corriente = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].tipoCuenta === "sueldo") {
+            sueldo.push(array[i])
+        }
+        
+        else if (array[i].tipoCuenta === "corriente") {
+            corriente.push(array[i])
+        }
+    }
+    console.log(sueldo)
+    console.log(corriente)
+    
+}
+tiposDeCta(arrayCuentas)
+
+    // Obtener un nuevo array con las cuentas habilitadas.
+    console.log("ejerciciooo")
+    console.log("ejerciciooo")
+    console.log("ejerciciooo")
+
+function cuentaHabilitada(array) {
+    let cuentaHab = []
+    for (let i = 0; i < array.length; i++) { 
+        if (array[i].estaHabilitada === true) {
+            cuentaHab.push(array[i])
+        }
+    }
+    return cuentaHab
+}
+console.log(cuentaHabilitada(arrayCuentas))
+console.log("ejerciciooo")
+
+    // Obtener un nuevo array con las cuentas deshabilitadas.
+
+function cuentaDeshabilitada(array) {
+    let cuentaDesH = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].estaHabilitada === false) {
+            cuentaDesH.push(array[i])
+        }
+    }
+    return cuentaDesH
+}
+console.log(cuentaDeshabilitada(arrayCuentas))
+
+    // Obtener la cuenta con el menor saldo.
+
+    function menorSaldo(array) {
+        let cuentaMenorSaldo = []
+    for (let i = 0; i < array.length; i++ ) {
+        cuentaMenorSaldo.push(array[i].saldo)
+    }
+    for (let i = 0; i < array.length; i++ ) {
+        if (array[i].saldo === Math.min(...cuentaMenorSaldo)){    
+    return array[i]
+}
+}
+}
+console.log(menorSaldo(arrayCuentas))
+
+    // Obtener la cuenta con el mayor saldo.
+      
+
